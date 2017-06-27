0c31a73ff40fbe3c14c8a895b564cdc0df1d9b3f
Sebastian Ariel Gambolati <sebastian.gambolati@southworks.com>
2017-06-27 09:45:15 -0300

    Initial Commit.

---
+++ Assets/Scripts/VoiceMover.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class VoiceMover : MonoBehaviour
+{
+    public float MovementVelocity = 30f;
+
+    private TextToSpeechManager textToSpeechManager;
+
+    public void MoveForward()
+    {
+        this.transform.Translate(Vector3.forward * Time.deltaTime * this.MovementVelocity);
+        this.textToSpeechManager.SpeakText("Moving forward, sir!");
+    }
+
+    public void MoveBackward()
+    {
+        this.transform.Translate(Vector3.back * Time.deltaTime * this.MovementVelocity);
+        this.textToSpeechManager.SpeakText("Moving backward, sir!");
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        this.textToSpeechManager = this.GetComponent<TextToSpeechManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
